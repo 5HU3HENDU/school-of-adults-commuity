9e7b9417e6921a581b73ce0aafe830744ef12c42
Shubhendu Singh <shubhendusingh1505@gmail.com>
2025-05-10 22:39:35 +0530

    Update sofa.py

--- sofa.py
+++ sofa.py
@@ -8,20 +8,20 @@ health = st.Page("pages/health.py")
 
 pg = st.navigation(
 	{
-		"Home": [homepage],
-		"Health": [health],
-		"Wealth": [],
-		"Career": [],
-		"Home Management": [],
-		"People Management": [],
-		"Pets": [],
-		"Travel": [],
-		"Content": [],
-		"Fashion": [],
-		"Life Skills": [],
-		"Life Events": [],
-		"Tech Management": [],
-		"Legal": []
+		"Home": [homepage], 
+		"Health": [food, exercise, body_parts, health_checkup, mental_health ],
+		"Wealth": [earn, stocks, mutual_fund, insurance, spend, track],
+		"Career": [find_job, career_ladder, resignation],
+		"Home Management": [kitchen, bedroom, living_room, bathroom, garden],
+		"People Management": [self, family, parent, kids, partner, friends, enemy, office],
+		"Pets": [cats, dogs],
+		"Travel": [place, stay, flight, train, bus, cab, itinerary],
+		"Content": [news, movies, song. book],
+		"Fashion": [men, women, kid],
+		"Life Skills": [driving, swimming, problem_solving],
+		"Life Events": [school, college, marriage, buying_home],
+		"Tech Management": [mobile, tv, earphone, power_bank],
+		"Legal": [basic_legal]
 		
         }
     )
