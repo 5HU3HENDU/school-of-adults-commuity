7afe71510f30f91a3134604bb2732ef23ea4eb6f
Shubhendu Singh <shubhendusingh1505@gmail.com>
2025-05-10 15:05:10 +0530

    Update sofa.py

--- sofa.py
+++ sofa.py
@@ -2,11 +2,14 @@ import streamlit as st
 
 homepage = st.Page("pages/homepage.py", title="School Of Adults", icon="🌉", default = True)
 
+health = st.Page("pages/health.py", title="Health", icon=":dna:")
+
 
 
 pg = st.navigation(
 	{
-            "Home": [homepage]
+		"Home": [homepage],
+		"Health":[health]
         }
     )
 
