18d17ad72400f271b6354ae4c7f3aeb5d3f55ca3
Shubhendu Singh <shubhendusingh1505@gmail.com>
2025-05-19 23:56:41 +0530

    Update homepage.py

--- pages/homepage.py
+++ pages/homepage.py
@@ -7,3 +7,33 @@ st.set_page_config(
 st.title(":school: SofA")
 
 st.header("School Of Adults")
+
+st.divider()
+
+# Insert wordcloud
+
+"""
+School of Adults, is a community based platform to navigate every aspect of adulthood!
+
+The idea is to question, discuss and document everything which is _needed_ to live a noise-free and quality life.
+"""
+
+st.subheader("Philosophy")
+
+"""
+- School time was fun but it didn't prepare us for the real world.
+- If we know the question, we can get the answer using Google/AI tools.
+- **But what if we don't even know the questions?**
+- Everyone is great in something.
+"""
+
+
+st.code("""SofA = Quality People
+        + Different Backgrounds
+        + Versatile Domains
+        + Community
+        + Structured Discussions
+        + Neat Documentation
+        """)
+
+
