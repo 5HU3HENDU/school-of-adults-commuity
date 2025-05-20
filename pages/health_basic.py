3481e4d5b1c38917fe254fc05eefeee8a0920b9a
Shubhendu Singh <shubhendusingh1505@gmail.com>
2025-05-20 23:05:56 +0530

    Update health_basic.py

--- pages/health_basic.py
+++ pages/health_basic.py
@@ -1,9 +1,75 @@
 import streamlit as st
 
+st.set_page_config(
+    layout="wide"   
+)
 
+st.title("1️⃣ Health - Basics")
 
+st.divider()
 
+st.header("Emergency")
 
+"""
+- [Call an ambulance - 102](tel:102)
+- [Hospitals Near Me](https://www.google.com/maps/search/hospitals+near+me/)
+"""
 
+st.header("Medical Help")
+"""
+- [Healthcare Chat Bot](https://www.meetaugust.ai/)
+- [Talk to a doctor - online](https://www.apollo247.com/specialties)
+"""
 
+st.header("Early Health Signs to Watch Out For")
+"""
+- Unexplained weight loss or gain
+- Persistent fatigue or lack of energy
+- Changes in appetite or eating habits
+- Chronic pain or discomfort
+- Difficulty sleeping or changes in sleep patterns
+- Mood swings or sudden changes in emotions
+- Persistent cough or sore throat
+- Frequent headaches or migraines
+- Digestive issues or changes in bowel movements
+- Skin changes or unusual moles
+"""
+
+st.header("Things to Avoid")
+"""
+- Excessive smoking
+- Excessive alcohol consumption
+- Sedentary lifestyle
+- Poor diet high in processed foods
+- Lack of sleep
+- Stress
+- Excessive sugar intake
+- Not staying hydrated
+- Ignoring regular exercise
+- Neglecting mental health
+"""
+
+st.header("Top Reasons of Death")
+"""
+- Cardiovascular diseases
+- Cancer
+- Respiratory diseases
+- Lower respiratory infections
+- Alzheimer's disease and other dementias
+- Diabetes mellitus
+- Kidney diseases
+- Liver diseases
+- Digestive diseases
+- Accidents (unintentional injuries)
+"""
+
+st.header("Medicines to Keep at Home")
+"""
+- Headache
+- Fever
+- Stomach Pain
+- Indigestion
+- First Aid Kit
+- Cough
+"""
 
