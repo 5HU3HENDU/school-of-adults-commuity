edd0594fb02843b59fd833066e7ed03e7791a637
Shubhendu Singh <shubhendusingh1505@gmail.com>
2025-05-22 00:41:32 +0530

    Update and rename wealth_earn.py to wealth_create.py

---
+++ pages/wealth_create.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+st.set_page_config(
+    layout="wide"   
+)
+
+st.title("🪙 Generating Wealth")
+
+st.divider()
+
+st.header("Side Hustle - Passive Income")
+
+st.subheader("Housemakers as day trader")
+"""
+- One way to manage and grow wealth is by enabling housemakers to do active trading or investment in stocks.
+- It will help them feel valuable and also will help manage family wealth effectively.
+- They will require some training and experience but that applies to everyone.
+"""
+
+st.subheader("Other Resources")
+"""
+- [Topmate](https://topmate.io/)
+- [Gumroad](https://gumroad.com/)
+- [Vistaprint](https://www.vistaprint.in/)
+- [Blinkstore](https://blinkstore.in/)
+"""
