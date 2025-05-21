a4cf4f394a15abe336310666a487085f33afe159
Shubhendu Singh <shubhendusingh1505@gmail.com>
2025-05-22 00:42:16 +0530

    Update and rename wealth_stocks.py to wealth_invest.py

---
+++ pages/wealth_invest.py
@@ -0,0 +1,72 @@
+import streamlit as st
+
+st.set_page_config(
+    layout="wide"   
+)
+
+st.title("💹 Investing")
+
+st.divider()
+
+st.header("Learn to invest")
+"""
+- [Zerodha Varsity](https://zerodha.com/varsity/)
+"""
+
+st.header("Mutual Fund")
+"""
+- [MF Central](https://www.mfcentral.com/)
+- [List of Mutual Funds](https://groww.in/mutual-funds/filter)
+"""
+
+st.header("Stocks")
+"""
+- [Zerodha](https://zerodha.com/)
+- [Smallcase](https://www.smallcase.com/)
+- [Screener](https://screener.in/)
+- [Tijori](https://www.tijorifinance.com/)
+"""
+
+st.header("IPOs")
+"""
+- [Upcoming & recent IPOs](https://groww.in/ipo)
+"""
+
+st.header("Fixed Deposits - FD")
+"""
+- [Stable Money](https://stablemoney.in/)
+"""
+
+st.header("Gold")
+"""
+- [India Gold](https://indiagold.co/digital-gold)
+"""
+
+st.header("Real Estate")
+"""
+- [Holiday Homes](https://www.alyf.in/)
+- [Residential](https://www.indextap.com/)
+"""
+
+st.header("Pension")
+"""
+- [National Pension Scheme](https://enps.nsdl.com/eNPS/NationalPensionSystem.html)
+"""
+
+st.header("Provident Fund - PF")
+"""
+- [Employee Provident Fund - EPF](https://www.epfindia.gov.in/site_en/index.php)
+"""
+
+st.header("ULIP")
+"""
+- [Know About ULIP](https://www.etmoney.com/learn/insurance/all-you-need-to-know-about-unit-linked-insurance-plans/)
+"""
+
+
+
+
+
+
+
+
