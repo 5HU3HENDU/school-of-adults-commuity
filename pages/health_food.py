89122bbff3165ef475dcedf2ff55a93f8765112e
Shubhendu Singh <shubhendusingh1505@gmail.com>
2025-05-21 20:48:09 +0530

    Update health_food.py

--- pages/health_food.py
+++ pages/health_food.py
@@ -1,17 +1,31 @@
 import streamlit as st
 
-
 st.set_page_config(
     layout="wide"   
 )
 
-st.title("🍱 Food and Water | SofA")
+st.title("🍱 Food, Water and Air")
 
 st.divider()
 
+st.header("Air")
+"""
+- Use breathable mask whenever possible
+- Get an Air Purifier
+"""
+
 st.header("Water")
+"""
+- [Know your water](https://www.peore.in/blogs/all-blogs/know-your-water-a-complete-guide-to-drinking-water-quality)
+- Drink at least 4-5 litre of water everyday
+- Buy a water purifier if needed
+- Sometimes, if possible, you can also order Bisleri at home
+"""
 
-
-
-
-
+st.header("Food")
+"""
+- Diet Plan
+- Home cooked foods
+- Fruits
+- Dry Fruits
+"""
