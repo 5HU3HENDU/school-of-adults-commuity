a6bd2d4591b7c926ba4dcf8dbec0876b01020f24
Shubhendu Singh <shubhendusingh1505@gmail.com>
2025-05-22 00:44:00 +0530

    Update and rename wealth_mf.py to wealth_loans.py

---
+++ pages/wealth_loans.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+st.set_page_config(
+    layout="wide"   
+)
+
+st.title("💳 Credit Card and Loans")
+
+st.divider()
+
+st.header("Credit Score")
+"""
+- [CIBIL](https://www.cibil.com/freecibilscore)
+"""
+
+st.header("Credit Card")
+"""
+- [All Credit Cards](https://select.finology.in/credit-card)
+"""
+
+st.header("Loans")
+"""
+- Avoid taking an online loan
+- Always contact your bank first
+"""
