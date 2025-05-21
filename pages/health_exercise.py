edecb2cef1a2316cd943a845c1e47de4cff7bdb0
Shubhendu Singh <shubhendusingh1505@gmail.com>
2025-05-21 20:48:42 +0530

    Update health_exercise.py

--- pages/health_exercise.py
+++ pages/health_exercise.py
@@ -1,3 +1,20 @@
 import streamlit as st
 
-st.title("Exercise | SofA")
+st.set_page_config(
+    layout="wide"   
+)
+
+st.title("🧘 Exercise and Fitness")
+
+st.divider()
+
+st.header("Things to do")
+
+"""
+- Walking
+- Running
+- [Yoga](https://pocketyoga.com/pose/)
+- [Gym](https://musclewiki.com/)
+- Outdoor Sports such as Swimming, Cricket, Football etc.
+
+"""
