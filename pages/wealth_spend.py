fadb9ff6809b643962e29667674012c79b6cb00b
Shubhendu Singh <shubhendusingh1505@gmail.com>
2025-05-22 00:43:17 +0530

    Update wealth_spend.py

--- pages/wealth_spend.py
+++ pages/wealth_spend.py
@@ -1,3 +1,37 @@
 import streamlit as st
 
-st.title("Spending | SofA")
+st.set_page_config(
+    layout="wide"   
+)
+
+st.title("💸 Spending & Tracking")
+
+st.divider()
+
+
+st.header("Handling Multiple Bank Accounts")
+"""
+- Keep a separate bank account for online transactions and day to day spends.
+- From your salary bank account or other bank accounts, transfer 10k-20k to the online-only bank account as and when needed.
+- Use credit card for transactions whenever possible to keep your bank accounts secure.
+- This way, you are using online-only bank accounts + credit cards for your spends, while keeping your main bank accounts as secure as possible.
+- This also helps you in budgeting to some extent.
+"""
+
+st.header("Spend Management")
+"""
+- Through Apps
+- Using Excel
+- By using one bank account/wallet for all spends
+"""
+
+st.header("Lending Money")
+"""
+- Do not lend money to your friends, relatives or anyone at all.
+- If you still want to help, help them get a loan.
+"""
+
+st.header("Financial Planning")
+"""
+- [Financial Planning Platform](https://1finance.co.in/)
+"""
