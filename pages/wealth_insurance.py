8e9219108656322e7de1c294862ffbfe1d96cf96
Shubhendu Singh <shubhendusingh1505@gmail.com>
2025-05-22 00:42:50 +0530

    Update wealth_insurance.py

--- pages/wealth_insurance.py
+++ pages/wealth_insurance.py
@@ -1,3 +1,20 @@
 import streamlit as st
 
-st.title("Insurance | SofA")
+st.set_page_config(
+    layout="wide"   
+)
+
+st.title("🧿 Insurance & Tax")
+
+st.divider()
+
+"""
+- [Term Life Insurance](https://joinditto.in/life-insurance/articles/term-insurance-101/)
+- [All other insurance](https://www.hdfcergo.com/)
+"""
+
+
+st.header("Income Tax")
+"""
+- [Cleartax](https://cleartax.in/income-tax-efiling)
+"""
