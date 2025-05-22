33afc3903dce3cd53172e80d3c6718b8b424da20
Shubhendu Singh <shubhendusingh1505@gmail.com>
2025-05-22 23:13:24 +0530

    Update travel_stay.py

--- pages/travel_stay.py
+++ pages/travel_stay.py
@@ -1,3 +1,31 @@
 import streamlit as st
 
-st.title("Stay | SofA")
+st.set_page_config(
+    layout="wide"   
+)
+
+st.title("🏕️ Stay")
+
+st.divider()
+
+st.header("Hotels")
+"""
+- [Moustache](https://moustachescapes.com/)
+- [Bloom](https://staybloom.com/)
+- [FabHotels](https://www.fabhotels.com/)
+- [Postcards](https://www.postcardresorts.com/)
+"""
+
+
+
+st.header("Hostels")
+"""
+- [HostelWorld](https://www.hostelworld.com/)
+- [Zolo](https://zolostays.com/)
+- [Zostel](https://www.zostel.com/)
+- [Hosteller](https://www.thehosteller.com/)
+
+"""
+
+st.header("Like AirBnB")
+
