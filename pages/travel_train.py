676f2c0117ae87a37a845704ef7c117bbd0881da
Shubhendu Singh <shubhendusingh1505@gmail.com>
2025-05-22 23:14:13 +0530

    Update travel_train.py

--- pages/travel_train.py
+++ pages/travel_train.py
@@ -1,3 +1,26 @@
 import streamlit as st
 
-st.title("Train | SofA")
+st.set_page_config(
+    layout="wide"   
+)
+
+st.title("🚂 Train")
+
+st.divider()
+
+st.header("Booking Ticket")
+"""
+- [IRCTC](https://www.irctc.co.in/nget/train-search)
+- [AskDisha](https://askdisha.irctc.co.in/)
+"""
+
+st.header("Tracking")
+"""
+- [Train Status](https://www.google.com/search?q=Train+Status)
+"""
+
+st.header("Food Inside Train")
+"""
+- [Zoop](https://www.zoopindia.com/)
+"""
+
