2bed898f6bbcb6168e55a4303fdb47ce2a8db775
Shubhendu Singh <shubhendusingh1505@gmail.com>
2025-05-22 23:13:49 +0530

    Update travel_flight.py

--- pages/travel_flight.py
+++ pages/travel_flight.py
@@ -1,3 +1,19 @@
 import streamlit as st
 
-st.title("Flight | SofA")
+st.set_page_config(
+    layout="wide"   
+)
+
+st.title("✈️ Flight")
+
+st.divider()
+
+"""
+- [Book Flights](https://www.ixigo.com/flights)
+"""
+
+st.header("Things to avoid")
+"""
+- Do not buy tickets and book hotels before having the visa.
+
+"""
