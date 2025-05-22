202383e39ab6c9df903d7c129831df4acbe6b16e
Shubhendu Singh <shubhendusingh1505@gmail.com>
2025-05-22 23:15:31 +0530

    Update travel_itinerary.py

--- pages/travel_itinerary.py
+++ pages/travel_itinerary.py
@@ -1,2 +1,38 @@
 import streamlit as st
 
+st.set_page_config(
+    layout="wide"   
+)
+
+st.title("📋 Itinerary & Planning")
+
+st.divider()
+
+st.header("Long Weekends")
+
+st.header("Tools")
+"""
+- [Travlcase](https://www.travelcase.club/)
+- [Roam Around](https://www.roamaround.io/)
+- [Wonderplan](https://wonderplan.ai/)
+- [Maya](https://www.livetheworld.com/maya/chat)
+- [Vacay](https://www.usevacay.com/chatbot)
+- [WikiVoyage](https://en.m.wikivoyage.org/wiki/Main_Page)
+
+"""
+
+st.header("Train vs Flight")
+"""
+- If travel by train takes around 8 hours and one can get the night train, then prefer train over flight because one can easily spend the night in the train journey.
+- If money is not a concern, then obviously flight is the preferred choice.
+"""
+
+st.header("Travel Packags")
+"""
+- [IRCTC Tourism](https://www.irctctourism.com/)
+- [YHA](https://www.yhaindia.org/)
+- [No Footprints](https://www.nfpexplore.com/)
+- [Sterling Holidays](https://www.sterlingholidays.com/)
+- [Country Club](https://countryclubindia.net/)
+- [Skyway](https://www.skywaytour.com/)
+"""
