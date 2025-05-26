226c99badad87c156696a6b25a7e5806448ab1e2
Israamanaziz <azizisra2001@gmail.com>
2025-05-26 14:30:54 +0530

    Update pets_cats.py

--- pages/pets_cats.py
+++ pages/pets_cats.py
@@ -1,4 +1,57 @@
+
 import streamlit as st
 
-st.title("Cats | SofA")
-st.header("Meow🐱")
+st.set_page_config(
+    layout="wide"   
+)
+
+st.title("🐱Cats | SofA")
+
+st.divider()
+
+st.header("Vet Shops Near Me")
+"""
+- [Vet Shops](https://www.google.com/maps/search/Vet+Shops+Near+Me)
+"""
+
+st.header("Best Food Brands")
+"""
+- [Royal Canin](https://www.royalcanin.com/in/cats/products/retail-products)
+- [Purina Pro Plan](https://www.purina.in/cat/cat-food)
+- [Farmina Matisse](https://www.farmina.com/in/eshop-c-Cat-food.html?_gl=1*12x562r*_up*MQ..*_gs*MQ..*_ga*MTkwODQ1ODE4NC4xNzQ4MjQ3MjAy*_ga_CHZ53B5H79*czE3NDgyNDcyMDEkbzEkZzAkdDE3NDgyNDcyMTAkajAkbDAkaDE3MTIyMzY3MzEkZFJxQnFMeDVEaWNaZDlpSk9hbkhHNE5WTTkweWxkSERtYmc.&gclid=Cj0KCQjwotDBBhCQARIsAG5pinMtS-5qgdX29aI13MQSI9RvcRkDv4qcn8xosomUM1jvHP18F-CUp5AaAgCOEALw_wcB&gbraid=0AAAAABjG6dqu1snDZe0EeZhjFf7PBeB3u)
+- [Whiskas](https://www.whiskas.in/our-products/wet-cat-food)
+- [Sheba](https://www.sheba.in/)
+- [Me-O](https://www.meocatfood.com/?srsltid=AfmBOorJg6tldgaEbwVHg58OuW_Bp4C-LxuEDoo7AD4v9YgO3odqH7ao)
+- [Kennel](https://kennelkitchen.com/collections/cat-food)
+- [Drools](https://drools.com/for-cats/)
+- [Kitty Yum](https://kittyyums.com/products/)
+"""
+
+st.header("Grooming Brands")
+"""
+- [HUFT](https://headsupfortails.com/)
+- [Trixie](https://www.trixie.de/en/productworld/cat)
+- [Petkin](https://www.petkin.com/Cat-Care-Products-s/1821.htm)
+- [Himalaya](https://himalayawellness.in/collections/companion-care)
+- [Beaphar](https://www.beaphar.com/cat)
+"""
+
+st.header("Online Stores")
+"""
+- [PetsWorld](https://petsworld.in/collections/cats)
+- [HUFT](https://headsupfortails.com/?srsltid=AfmBOoo3Tte9oS6bKi_OFWinaKX3yQmT4YMixnsQml5rNklzYalHUnoU)
+- [Petsy](https://petsy.online/collections/cat-supplies)
+- [AmazonIndia](https://www.amazon.in/Cats-Supplies/b/ref_=catsupseoain?ie=UTF8&node=4771341031)
+- [Flipkart](https://www.flipkart.com/pet-supplies/cats/pr?sid=p3t,0mo)
+- [BearHugs](https://bearhugsofficial.com/?srsltid=AfmBOoqIXa_Jh-zq3pxYMAPEAGJl4bahnWf4WaOAkHxpb_ggHih7_vou)
+- [PetShopIndia](https://petshopindia.com/Cat)
+"""
+
+st.header("Cat Adoption Platform")
+"""
+- [ThePetNest](https://thepetnest.com/adopt-a-cat#/)
+- [CUPA Bangalore](https://cupabangalore.org/)
+- [RESQ Charitable Trust](https://www.resqct.org/)
+- [Blue Cross of India](https://bluecrossofindia.org/Adopt-Foster.html)
+"""
+
