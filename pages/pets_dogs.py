dbf2d3fa073059ff117f49f2eab1b1a38f6b0b26
Israamanaziz <azizisra2001@gmail.com>
2025-05-26 15:23:01 +0530

    Update pets_dogs.py

--- pages/pets_dogs.py
+++ pages/pets_dogs.py
@@ -1,3 +1,57 @@
 import streamlit as st
 
-st.title("Dogs | SofA")
+st.set_page_config(
+    layout="wide"   
+)
+
+st.title("🐶Dogs | SofA")
+
+st.divider()
+
+st.header("Vet Shops Near Me")
+"""
+- [Vet Shops](https://www.google.com/maps/search/Vet+Shops+Near+Me)
+"""
+
+st.header("Best Dog Food Brands")
+"""
+- [Royal Canin](https://www.royalcanin.com/in/dogs/products/retail-products)
+- [Pedigree](https://www.pedigree.in/)
+- [Farmina N&D](https://www.farmina.com/in/eshop-d-Dog-food.html?_gl=1*1fd6fgg*_up*MQ..*_gs*MQ..*_ga*OTg4Njg4OTQ3LjE3NDgyNTE4Njc.*_ga_CHZ53B5H79*czE3NDgyNTE4NjckbzEkZzAkdDE3NDgyNTE5NjgkajAkbDAkaDE4NTk5NDY5NTMkZDc5SWJweTNnSFpySS1kWDI4ajY2bnptNmNZUm1XSk04T2c.&gclid=Cj0KCQjwotDBBhCQARIsAG5pinPc0xE9jkZ0SleQ9-YCCzmdQ_fgCveca6MO8KoxM5vYI5eOG1cODgAaAveGEALw_wcB&gbraid=0AAAAABjG6dqu1snDZe0EeZhjFf7PBeB3u)
+- [PurePet](https://www.purepet.in/products.html)
+- [Fidele](https://fidele.co/?srsltid=AfmBOoqUQaXlWPF_iTO-7gGmwA-QyocnPKij5t2CSSti-mwqL-HhnvFi)
+- [Arden Grange](https://www.ardengrange.com/dogs/dryfood/puppy.html)
+- [Drools](https://drools.com/for-dogs/)
+"""
+
+st.header("Grooming Brands")
+"""
+- [HUFT](https://headsupfortails.com/?srsltid=AfmBOoo3Tte9oS6bKi_OFWinaKX3yQmT4YMixnsQml5rNklzYalHUnoU)
+- [Trixie](https://www.trixie.de/en/dog)
+- [Petkin](https://www.petkin.com/Dog-Care-Products-s/1820.htm)
+- [Beaphar](https://www.beaphar.com/dog)
+- [EarthBath](https://www.justdogsstore.com/product-category/brands/earthbath/?srsltid=AfmBOoqQmzbvTpjFV6mJH9ZYzBXWK5CcR-rt7lbl6oM1JZSg_WC5DBYQ)
+- [Himalaya Pet Care](https://himalayawellness.in/collections/companion-care)
+"""
+
+st.header("Online Stores")
+"""
+- [SuperTails](https://supertails.com/pages/dog-products)
+- [HUFT](https://headsupfortails.com/?srsltid=AfmBOoo-7p4OirPTpHZSKatxCJUhcO9zDFhVAR08A98CM58oIi2lOgmi)
+- [JustDogs](https://www.justdogsstore.com/)
+- [AmazonIndia](https://www.amazon.in/Dogs-Supplies/b/ref_=dogsupseoain?ie=UTF8&node=4771342031)
+- [Petsy](https://petsy.online/collections/dogs)
+- [PetsWorld](https://petsworld.in/collections/dogs)
+- [Flipkart](https://www.flipkart.com/pet-supplies/dogs/pr?sid=p3t,ea3)
+- [PawsIndia](https://pawsindia.com/collections/dog)
+"""
+
+st.header("Dog Adoption Platform")
+"""
+- [RESQ Charitable Trust](https://www.resqct.org/)
+- [Blue Cross Of India](https://bluecrossofindia.org/)
+- [Red Paws Rescue](https://www.redpawsrescue.com/adopt)
+- [CUPA Banglore](https://cupabangalore.org/adopt/)
+- [Stray Animal Foundation India](https://strayanimalfoundationindia.org/adopt/)
+- [Animal Aid Unlimited](https://www.animalaidunlimited.org/how-to-help/adopt-an-animal/)
+"""
