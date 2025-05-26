c7e062afab09011e47fb7de2e10d117f7fe92122
Israamanaziz <azizisra2001@gmail.com>
2025-05-26 19:01:49 +0530

    Update career_job.py

--- pages/career_job.py
+++ pages/career_job.py
@@ -1,3 +1,51 @@
 import streamlit as st
 
-st.title("Finding Job | SofA")
+st.set_page_config(
+    layout="wide"   
+)
+
+st.title("💼 Finding Jobs | SofA")
+
+st.divider()
+
+st.header("Advanced Job Search Tool")
+"""
+- [Job Search SofA](https://job-search-sofa.replit.app/)
+"""
+
+st.header("Job Websites")
+"""
+- [Pesto Tech](https://pesto.tech/)
+- [Workassist](https://workassist.in/)
+- [Instahyre](https://www.instahyre.com/)
+- [Cutshort](https://cutshort.io/)
+- [Y Combinator's Work at a Startup](https://www.workatastartup.com/)
+- [WellFound (Formerly AngelList Talent)](https://wellfound.com/jobs)
+- [Unstop](https://unstop.com/job-portal)
+- [Talent 500](https://talent500.co/)
+- [Expertia AI](https://expertia.ai/jobs)
+- [AmbitionBox](https://www.ambitionbox.com/jobs)
+- [apna.co](https://apna.co/)
+- [Uplers](https://www.uplers.com/talent/)
+"""
+
+st.header("Remote Jobs")
+"""
+- [Remote Rocketship](https://www.remoterocketship.com/)
+- [RemoteIndian](https://remoteindian.com/)
+- [Remote](https://remote.com/jobs)
+- [Himalayas](https://himalayas.app/jobs)
+- [The Muse](https://www.themuse.com/hiring/)
+- [Turing Jobs](https://www.turing.com/jobs)
+- [We Work Remotely](https://weworkremotely.com/)
+- [Airtable](https://airtable.com/app3xoWSiBIPD7oAB/shr0UmRIpI6nX9qi6/tblggGykJ8UJGeBGU/viwvTkpBC9eisSZ3e?blocks=hide)
+"""
+
+st.header("Communities")
+"""
+- [PeerList](https://peerlist.io/)
+- [Grapevine](https://grapevine.in/)
+- [Fishbowl](https://www.fishbowlapp.com/)
+- [Worktogether](https://worktogether.io/)
+"""
+
