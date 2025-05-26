5a15305717f262bc2ffbdd8fc75cb537bead0621
Israamanaziz <azizisra2001@gmail.com>
2025-05-26 16:57:32 +0530

    Update health_periods.py

--- pages/health_periods.py
+++ pages/health_periods.py
@@ -1,2 +1,71 @@
 import streamlit as st
 
+st.set_page_config(
+    layout="wide"   
+)
+
+st.title("♀️Periods | SofA")
+
+st.divider()
+
+st.header("Period Essentials")
+"""
+- Sanitary Pads
+- Tampons
+- Menstrual Cups
+- Period Panties
+- Reusable cloth Pads
+"""
+
+st.header("Trusted Period Brands")
+"""
+- [Whisper](https://whisper.co.in/en-in/sanitary-pads)
+- [Sofy](https://www.sofy.in/products/)
+- [Plush](https://www.plushforher.com/collections/shop-all)
+- [Sirona](https://thesirona.com/collections/period-care)
+- [Stayfree](https://www.stayfree.in/products)
+- [Pee Safe](https://www.peesafe.com/collections/menstrual-care)
+- [Carmesi](https://mycarmesi.com/collections/period-care)
+- [Nua](https://nuawoman.com/sanitary-pads)
+- [Niine](https://www.niine.com/)
+
+"""
+
+st.header("Period Pain Relief")
+"""
+- Hot water bags
+- Cramp roll-ons: Peesafe feminine, Nua, Sirona roll ons
+- Meftal Spas
+- Dark Chocolate
+- Fruits: Bananas, Pomegranates, berries, Water-rich fruits, Papayas
+- Herbal Teas: Ginger tea, Chamomile tea, Shatavari, Red raspberry and Ashoka tea.
+- Yoga Poses: Balasana(Child Pose), Uttanasana(Forward bend), 
+  Baddha Konasana, Shavasana.
+"""
+
+st.header("Apps For Tracking")
+"""
+- [Clue](https://helloclue.com/)
+- [Flo](https://flo.health/)
+- [Maya](https://www.maya.live/eng.html)
+- [Euki](https://eukiapp.org/)
+"""
+
+st.header("Educational Resources")
+"""
+- [Menstrupedia](https://www.menstrupedia.com/quickguide/girls)
+- [OHSU](https://www.ohsu.edu/sites/default/files/2024-05/Period%20Education%20Guide%20Full%20final.pdf)
+- [Period](https://period.org/)
+- [UNICEF Menstrual Hygiene](https://www.unicef.org/media/91346/file/unicef-guide-menstrual-hygiene-materials-2019.pdf)
+"""
+
+st.header("Support & Awareness Groups")
+"""
+- [Sachhi Saheli](https://sachhisaheli.org/)
+- [Boondh](https://boondh.co/)
+- [Goonj](https://goonj.org/category/health/)
+- [Menstrupedia](https://www.menstrupedia.com/)
+- [Project Baala](https://projectbaala.com/)
+- [Pinkishe](https://www.pinkishe.org/)
+"""
+
