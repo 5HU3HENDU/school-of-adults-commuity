2e11fffd20a9baf068d884f594de960b23247070
Israamanaziz <azizisra2001@gmail.com>
2025-05-27 22:35:26 +0530

    Update career_choose.py

--- pages/career_choose.py
+++ pages/career_choose.py
@@ -1,3 +1,29 @@
 import streamlit as st
 
-st.title("Choosing Career | SofA")
+st.set_page_config(
+    layout="wide"   
+)
+
+st.title("🎓Choosing career | SofA")
+
+st.divider()
+
+st.header("Career Library")
+"""
+- [Mindler](https://www.mindler.com/careerlibrary)
+- [Truity](https://www.truity.com/search-careers/holland-code)
+- [National Career Service](https://www.ncs.gov.in/)
+- [MapMyTalent](https://www.mapmytalent.in/right-career)
+"""
+
+st.header("Study Abroad")
+"""
+- [IDP](https://www.idp.com/india/)
+- [Yocket](https://yocket.com/)
+- [Fateh Education](https://www.fateheducation.com/)
+- [AECC Global](https://www.aeccglobal.com/)
+- [SIEC India](https://www.siecindia.com/)
+- [Leapscholar](https://leapscholar.com/)
+
+"""
+
