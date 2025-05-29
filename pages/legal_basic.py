f7054158f5ba62bc196c35ceeea999c01e2ed4c0
Israamanaziz <azizisra2001@gmail.com>
2025-05-29 19:37:20 +0530

    Update legal_basic.py

--- pages/legal_basic.py
+++ pages/legal_basic.py
@@ -1,2 +1,42 @@
 import streamlit as st
 
+st.set_page_config(
+    layout="wide"   
+)
+
+st.title("⚖️ Legal | SofA")
+
+st.divider()
+
+st.header("Consult a Lawyer")
+"""
+- [Lawrato](https://lawrato.com/)
+- [vakilsearch](https://vakilsearch.com/)
+- [Legalkart](https://www.legalkart.com/)
+- [MyAdvo](https://www.myadvo.in/)
+
+"""
+
+st.header("Case Status")
+"""
+- [ecourt services](https://ecourts.gov.in/ecourts_home/)
+- [High Court](https://hcservices.ecourts.gov.in/hcservices/main.php)
+- [Supreme Court](https://www.sci.gov.in/)
+
+"""
+
+st.header("Legal Information & Judgements")
+"""
+- [Indiankanoon](https://indiankanoon.org/)
+- [Nyaaya](https://nyaaya.org/)
+
+"""
+
+st.header("Legal Aid and Rights Awareness")
+"""
+- [National Legal Services Authority (NALSA)](https://nalsa.gov.in/)
+- [Probono India](https://www.probono-india.in/)
+
+"""
+
+
