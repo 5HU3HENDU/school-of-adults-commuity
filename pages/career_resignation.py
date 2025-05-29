df79d134800299825d806547912b5e4a06fe50d8
Israamanaziz <azizisra2001@gmail.com>
2025-05-29 20:16:57 +0530

    Update career_resignation.py

--- pages/career_resignation.py
+++ pages/career_resignation.py
@@ -1,3 +1,30 @@
 import streamlit as st
 
 st.title("Resignation | SofA")
+
+st.divider()
+
+st.header("Retention vs Resignation")
+"""
+- Once you have the final offer letter, you can choose either to resign in the current organization or to ask your manager about retention.
+- Retention can help you stay in the same company with some higher package.
+- Be clear with your choice before talking to manager or HR about this.
+"""
+
+st.header("During Notice Period")
+"""
+- Once you have resigned, make sure you serve the complete notice period.
+- It helps you relax as workload will be lesser. Also, you'll be getting paid for un-applied privilege leaves in your full-and-final settlement.
+- But of course, choice of serving complete notice period depends on the individual.
+- Complete all the pending works and knowledge transfer on time.
+- Try not to spoil your relationship with people you have worked with.
+- Download all the pay slips and relevant documents before the last working day.
+- Leave gracefully.
+- Also, you can interview for more opportunities parallelly, given you already have one offer.
+- Now you can be demanding and see where things might go.
+"""
+
+st.header("Off to a new start")
+"""
+After serving the notice period, a new adventure begins 🥂.
+"""
