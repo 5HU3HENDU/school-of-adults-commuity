087dfd38ca61eb2494166368d834543476bc89cb
Israamanaziz <azizisra2001@gmail.com>
2025-05-29 21:11:30 +0530

    Update career_ladder.py

--- pages/career_ladder.py
+++ pages/career_ladder.py
@@ -1,3 +1,21 @@
 import streamlit as st
 
-st.title("Career Ladder | SofA")
+st.title("🪜Career Ladder | SofA")
+
+st.divider()
+
+st.header("Upskilling Platforms")
+"""
+- [Imarticus Learning](https://imarticus.org/)
+- [upGrad](https://www.upgrad.com/)
+- [Scaler](https://www.scaler.com/)
+- [CrashCourse](https://www.youtube.com/@crashcourse/playlists?app=desktop)
+- [Board Infinity](https://www.boardinfinity.com/)
+"""
+
+st.header("Niche Upskilling Platforms")
+"""
+- [Consulting, Product and Analytics](https://altuni.in/)
+- [Data Science and AI](https://www.turingcollege.com/data-science)
+- [Python, Machine Learning, Deep Learning](https://jovian.com/)
+"""
