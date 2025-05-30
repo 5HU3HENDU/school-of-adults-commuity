eea3df12159cdfaecef52f432c0d2f6577aba36a
Israamanaziz <azizisra2001@gmail.com>
2025-05-30 17:52:56 +0530

    Update content_movies.py

--- pages/content_movies.py
+++ pages/content_movies.py
@@ -1,2 +1,13 @@
 import streamlit as st
 
+st.title("🍿Movies | SofA")
+
+st.divider()
+
+st.header("Movies & TV Series")
+"""
+- [Movies released in India](https://letterboxd.com/films/popular/this/month/year/2024/country/india/)
+- [Movies released Worldwide](https://letterboxd.com/films/popular/this/month/year/2024/)
+- [Streaming Services in One Place](https://reelgood.com/)
+"""
+
