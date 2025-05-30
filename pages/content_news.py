1fbca93a87ce51d07fa59d85958718872e88cbd3
Israamanaziz <azizisra2001@gmail.com>
2025-05-30 17:21:24 +0530

    Update content_news.py

--- pages/content_news.py
+++ pages/content_news.py
@@ -1,2 +1,21 @@
 import streamlit as st
 
+st.title("📰 News | SofA")
+
+st.divider()
+
+st.header("General")
+"""
+- [News Minimalist](https://www.newsminimalist.com/)
+- [Zee Business](https://www.youtube.com/@ZeeBusiness/streams)
+
+"""
+
+st.header("Sports")
+"""
+- 🏏Upcoming Cricket Matches in India
+  - [Cricbuzz](https://www.cricbuzz.com/cricket-team/india/2/schedule)
+- ⚽Upcoming Soccer Matches
+  - [ESPN- Soccer schedule](https://www.espn.in/football/fixtures)
+
+"""
