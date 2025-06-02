7aad397583e5241b3fafca974ec14acd951c97e0
Israamanaziz <azizisra2001@gmail.com>
2025-06-02 22:32:16 +0530

    Update home_garden.py

--- pages/home_garden.py
+++ pages/home_garden.py
@@ -1,3 +1,20 @@
 import streamlit as st
 
-st.title("Garden | SofA")
+st.title("🌱Garden | SofA")
+
+st.divider()
+
+"""
+
+- [Gardening Encyclopedia](https://gardening.bharatagri.com/pages/package-of-practices)
+
+"""
+
+st.header("Online Gardening Stores")
+
+"""
+- [Ugaoo](https://www.ugaoo.com/)
+- [Nurserylive](https://nurserylive.com/)
+- [Amazon India](https://www.amazon.in/s/ref=mega_sv_s23_8_1_1_1?rh=i%3Alawngarden%2Cn%3A3638775031&ie=UTF8&bbn=2454176031)
+
+"""
