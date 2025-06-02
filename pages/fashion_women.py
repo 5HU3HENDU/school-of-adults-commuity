bb125e98d7d9c7d8cc3b68548eea09eaacd99b87
Israamanaziz <azizisra2001@gmail.com>
2025-06-02 22:03:39 +0530

    Update fashion_women.py

--- pages/fashion_women.py
+++ pages/fashion_women.py
@@ -1,2 +1,66 @@
 import streamlit as st
 
+st.title("♀️For Women | SofA")
+
+st.divider()
+
+"""
+- [Personal Fashion Assistant](https://heyalle.com/)
+
+"""
+
+st.header("Fashion Hacks")
+"""
+- [Know your Shape](https://www.stitchfix.com/women/blog/fashion-tips/find-fit-for-your-body-type/?srsltid=AfmBOophAwRvAHwGXHL9yhoafoM_YwaPEpOvvZirPcuWRE66SuwwD-e6)
+- Avoid wardrobe fatigue by rotating accessories, not clothes.
+- Roll your clothes instead of folding them to save space and avoid wrinkles.
+- Match your belt and shoes to enhance your looks.
+- Use baby wipes to remove deodorant stains before heading out.
+- Double-sided tapes can fix gaping in the shirt, loose hems, etc.
+
+"""
+
+st.header("Closet and Visual Planning")
+
+"""
+- [Stylebook](https://www.stylebookapp.com/)
+- [Lookscope](https://lookscopeapp.com/#/)
+- [Cladwell](https://cladwell.com/)
+- [Canva](https://www.canva.com/)
+- [Pureple](https://pureple.com/)
+- [Pronti](https://www.pronti.com/)
+
+"""
+st.header("Shopping and Styling")
+
+"""
+- [FabIndia](https://www.fabindia.com/shop/women-products)
+- [VERO MODA](https://www.veromoda.in/collections/vm-all-product)
+- [Tatacliq](https://www.tatacliq.com/)
+- [Savana](https://www.savana.com/search/result/)
+- [Myntra](https://www.myntra.com/shop/women)
+- [H&M India](https://www2.hm.com/en_in/women/new-arrivals/view-all.html)
+- [Nykaa Fashion](https://www.nykaafashion.com/women/c/6557?root=topnav_1)
+- [Ajio](https://www.ajio.com/shop/women)
+- [Thesouledstore](https://www.thesouledstore.com/women)
+"""
+
+st.header("Affordable Skincare Products")
+"""
+- [Foxtale](https://foxtale.in/collections/skincare)
+- [Dot & Key](https://www.dotandkey.com/)
+- [Hyphen](https://letshyphen.com/)
+- [Earth Rhythm](https://earthrhythm.com/)
+- [Clinique](https://www.clinique.in/skincare-supercat)
+"""
+
+st.header("Cosmetics")
+"""
+- [Swiss beauty](https://swissbeauty.in/)
+- [MARS](https://marscosmetics.in/)
+- [MAC Cosmetics](https://www.maccosmetics.in/)
+- [Sugar Cosmetics](https://www.sugarcosmetics.com/)
+- [Colorbar](https://www.colorbarcosmetics.com/)
+- [Sephora](https://sephora.in/)
+"""
+
