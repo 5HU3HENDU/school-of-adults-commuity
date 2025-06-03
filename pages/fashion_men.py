ec9a33716d6185eeb5288e10f506f985486253a2
Israamanaziz <azizisra2001@gmail.com>
2025-06-03 22:47:17 +0530

    Update fashion_men.py

--- pages/fashion_men.py
+++ pages/fashion_men.py
@@ -1,2 +1,53 @@
 import streamlit as st
 
+st.title("♂️For Men | SofA")
+
+st.divider()
+
+"""
+- [Your Personal Fashion Assistant](https://heyalle.com/)
+
+"""
+
+st.header("Fashion Hacks")
+"""
+- Use collar stays or a trimmed paper/plastic strip to keep shirt collars crisp.
+- Buy one pair of glasses that fits your face, not trends.
+- For office, buy versatile basics in neutral colors that mix and match effortlessly.
+- Roll your clothes instead of folding them to save space and avoid wrinkles.
+- Match your belt and shoes to enhance your looks.
+- Use baby wipes to remove deodorant stains before heading out.
+- Double-sided tapes can fix gaping in the shirt.
+
+"""
+
+st.header("Closet and Visual Planning")
+
+"""
+- [Twelve70](https://www.twelve70.com/home)
+
+"""
+
+
+st.header("Custom-Made Clothing")
+
+"""
+- [Bombay Shirt Company](https://www.bombayshirts.com/)
+- [DaMENSCH](https://www.damensch.com/men/)
+- [Andamen](https://www.andamen.com/collections/shop-all-andamen?filter.v.availability=1)
+- [Herringbone and Sui](https://herringboneandsui.com/)
+
+"""
+st.header("Shopping and Styling")
+
+"""
+- [FabIndia](https://www.fabindia.com/shop/women-products)
+- [Bewakoof](https://www.bewakoof.com/men-clothing)
+- [Tatacliq](https://www.tatacliq.com/)
+- [Myntra](https://www.myntra.com/shop/men)
+- [H&M India](https://www2.hm.com/en_in/men/new-arrivals/view-all.html)
+- [Ajio](https://www.ajio.com/shop/men)
+- [Thesouledstore](https://www.thesouledstore.com/men)
+"""
+
+
