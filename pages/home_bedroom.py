5110f218bca4f0a144ae66fc572ccaa6b33feb3a
Israamanaziz <azizisra2001@gmail.com>
2025-06-05 17:39:14 +0530

    Update home_bedroom.py

--- pages/home_bedroom.py
+++ pages/home_bedroom.py
@@ -1,3 +1,35 @@
 import streamlit as st
 
-st.title("Bedroom | SofA")
+st.title("🛌 Bedroom | SofA")
+
+st.divider()
+
+st.header("Decluttering")
+"""
+- Keep a laundry basket in the bedroom to avoid piling up of clothes on the the bed or a chair.
+- Use a drawer organizer to divide chargers, medicines, undergarments and socks. 
+- Keep a tray or small box for keys, watch, wallet, and ID on the bedside table.
+- Water bottle/glass next to the bed.
+- Torch or emergency lights near the bed.
+- If space is tight, under-bed storage is a helpful option for storing seasonal items or extra stuffs.
+- Wash bedsheets weekly  and rotate between 3-4 sets.
+"""
+
+st.header("Bedroom setup")
+"""
+- Design your room using  [IKEA Kreativ](https://www.ikea.com/in/en/home-design/dashboard/).
+"""
+
+st.header("Bedroom Needs")
+"""
+- Use soft [warm LED bulbs](https://www.amazon.in/LED-Bulbs-Warm-White-Light/s?rh=n%3A4205458031%2Cp_n_feature_four_browse-bin%3A9956284031) for lamps or wall lights.
+- Use a [humidifier](https://www.amazon.in/Humidifiers/b?ie=UTF8&node=5403407031) to add moisture in the room. Use in dry season(winter)/AC to avoid dry throat, skin or sinus issues.
+- Use [Air purifiers](https://praan.io/hive/) with HEPA filters to remove pollution(dust, pet dander, pollen, etc).
+- [Bedroom plants](https://www.ugaoo.com/collections/bedroom?srsltid=AfmBOor4hO-X0rQ_qAHq62I5pdN83lGLsxX3Rwtr833CfJfKJ98m-iyK)
+"""
+
+st.header("Stores")
+"""
+- [IKEA](https://www.ikea.com/in/en/rooms/bedroom/)
+- [Sleepyhead](https://mysleepyhead.com/)
+"""
