902fe80211b8d352609946fe2558ae35d10db080
Israamanaziz <azizisra2001@gmail.com>
2025-06-05 16:48:13 +0530

    Update home_kitchen.py

--- pages/home_kitchen.py
+++ pages/home_kitchen.py
@@ -1,3 +1,40 @@
 import streamlit as st
 
-st.title("Kitchen | SofA")
+st.title("🍽️Kitchen | SofA")
+
+st.divider()
+
+st.header("Must-haves")
+"""
+- Pressure cooker, Kadhai, tava, saucepan
+- Big knife, Small knife, vegatable peeler, Chooping board
+- Spoons, Tongs, Ladles
+- Mixer-Grinder, Mortar and pestle, Mixing bowl
+- Transparent Storage containers with labels on them.
+
+- [Create a Functional Kitchen](https://www.cooksmarts.com/cooking-guides/create-a-functional-kitchen/20-must-have-kitchen-tools/)
+"""
+
+st.header("Pantry Essentials")
+"""
+- [cooksmarts](https://www.cooksmarts.com/cooking-guides/create-a-functional-kitchen/pantry-essentials/)
+"""
+
+st.header("Grocery Shopping")
+"""
+- Use grocery apps for noting down things to buy, by adding them into cart.
+- For shared grocery list with roommates and family, one can use google keep with checklists.
+- Buy in lesser amount, buy frequently.
+"""
+
+st.header("Recipe Helpers")
+"""
+- [Indian recipe finder](https://recipes.ankanroy.in/)
+- [The Curry Guy](https://greatcurryrecipes.net/category/indian-recipes/)
+- [Dassana's veg recipes](https://www.vegrecipesofindia.com/recipes/)
+"""
+
+st.header("Smart Cooking")
+"""
+- [Masala Lab](https://masalalab.in/)
+"""
