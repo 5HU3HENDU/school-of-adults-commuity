0572eea85d973626730db4dfdaee9ad3e14c3c23
Israamanaziz <azizisra2001@gmail.com>
2025-06-06 22:00:49 +0530

    Update home_bathroom.py

--- pages/home_bathroom.py
+++ pages/home_bathroom.py
@@ -1,3 +1,43 @@
 import streamlit as st
 
-st.title("Bathroom | SofA")
+st.set_page_config(
+    layout="wide")
+
+st.title("🛀🏻 Bathroom | SofA")
+
+st.divider()
+
+st.header("Bathroom Hacks")
+"""
+- If space is tight, use a towel ladder or towel rail instead of hooks
+- Keep one big bin with a lid.
+- To keep mold away and to reduce mineral buildup, spray white vinegar or sprinkle baking soda with salt in corners of the bathroom.
+- Keep a diffuser with lemon + eucalyptus oil to kill foul smell.
+- Stick or place naphthalene balls or camphor in corners to repel bugs.
+- Bathroom must always be equipped with floor wiper, bucket, mug, disinfectant, drain uncloggger and a liquid handwash.
+- Items for personal hygiene(Shampoo, soap, bodywash, facewash, razor) can always be kept separately in a wash bag to organize stuff effectively.
+"""
+
+st.header("Bathroom Needs")
+"""
+- [Geyser](https://www.vijaysales.com/c/geysers-and-water-heaters)
+- [Exhaust Fans](https://www.crompton.co.in/collections/exhaust-fans)
+- Mirrors
+- [Cabinets](https://www.ikea.com/in/en/cat/bathroom-products-ba001/)
+"""
+
+st.header("House Services")
+"""
+- [Urban Company- bathroom cleaning](https://www.urbancompany.com/near-me/bathroom-cleaning-services-near-me)
+- [NoBroker](https://www.nobroker.in/bathroom-cleaning-services-in-mumbai?isExperimental=true&sub_category=BATHROOM_CLEANING_SUBSCRIPTION_FORTNIGHTLY_6_MONTH&nbFr=hsSearch&)
+- [Safaiwale](https://safaiwale.in/bathroom-cleaning-services/)
+"""
+st.header("Stores")
+"""
+- [Hindware](https://hindware.com/)
+- [Homecentre](https://www.homecentre.in/in/en/search?q=bathroom)
+- [IKEA](https://www.ikea.com/in/en/cat/bathroom-products-ba001/)
+- [Urban Ladder](https://www.urbanladder.com/products/search?utf8=%E2%9C%93&keywords=bathroom)
+"""
+
+  
