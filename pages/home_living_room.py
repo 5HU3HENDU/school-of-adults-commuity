9572295a255e1b49a9b3ab2db8e1bbbd413f76c7
Israamanaziz <azizisra2001@gmail.com>
2025-06-06 20:28:22 +0530

    Update home_living_room.py

--- pages/home_living_room.py
+++ pages/home_living_room.py
@@ -1,3 +1,32 @@
 import streamlit as st
 
-st.title("Living Room | SofA")
+st.set_page_config(
+    layout="wide")
+
+st.title("📺Living Room | SofA")
+
+st.divider()
+
+st.header("Room Planner")
+"""
+- [IKEA Kreativ](https://www.ikea.com/in/en/planners/design-your-dream-home-pub66945dd9/#0846f8c0-b20d-11eb-8bb5-a73a5787c6e2)
+"""
+
+st.header(" House Services")
+"""
+- [Urban Company](https://www.urbancompany.com/)
+- [Guidance for perfect lighting](https://www.wiproconsumerlighting.com/custom-lighting-journey)
+"""
+
+st.header("Stores")
+"""
+- [IKEA](https://www.ikea.com/in/en/rooms/living-room/gallery/a-modern-living-room-bursting-with-life-pubceb77530/)
+- [Urban Ladder](https://www.urbanladder.com/seating?src=g_topnav_living_seating)
+"""
+
+st.subheader("Budget Furniture")
+"""
+- [OLX India](https://www.olx.in/en-in/furniture_c628)
+- [Quikr](https://www.quikr.com/home-lifestyle/furniture+Home-Office-Furniture+Bangalore+x218f?sx=true)
+"""
+        
