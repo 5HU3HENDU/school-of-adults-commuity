68582e38d58219a209a1aa4f40e7b57098f168f3
Israamanaziz <azizisra2001@gmail.com>
2025-06-09 20:06:53 +0530

    Update health_check.py

--- pages/health_check.py
+++ pages/health_check.py
@@ -4,10 +4,41 @@ st.set_page_config(
     layout="wide"   
 )
 
-st.title("🩺 Check Up")
+st.title("🩺 Check Up | SofA")
 
 st.divider()
 
 """
-- Frequent health chek-ups are mandatory.
+- Frequent health screening/check-ups are mandatory.
+- Always get tested at NABL/ISO certified Diagnostic Labs.
+- Don't trust a package blindly, instead ask yourself (Which of these tests add real clinical value for someone my age/gender/history)?
+- Always push for free Doctor consultation or clinical interpretation in labs where you got tested.
+- use Ayushman Bharat/ABHA ID to store reports digitally.
+- Get a routine check-up once or twice a year.
+"""
+
+st.header("Screening Checklists")
+"""
+- **0-10 year olds**: Vision, hearing, Vaccinations(per IAP schedue), dental checkups
+- **Teen and Young Adults**: CBC(Complete Blood Count), Lipid profile, Thyroid Total( T3, T4, TSH), Vitamin D and B12, BMI. 
+- **Adults(30+)**: Blood Sugar(FBS, HbA1c), Lipid Profile, LFT, RFT, ECG, Pap smear(Cervical cancer), Breast exam, BMI.
+- **Seniors(60+)**: ECG+TMT, PSA(Men), Mammogram(Women), Bone Density, Colon Cancer Screening, Cognitive screening(Dementia,MCI), health,weight: BMI monitoring.
+
+- **Universal tests**: Blood Pressure,BMI, Dental check-ups, HIV/Hepatitis B & C, Vaccines(Covid-19,Tetanus,etc)
+"""
+st.header("Home Sample Collection Services")
+"""
+- [Thyrocare](https://www.thyrocare.com/)
+- [SRL Diagnostics](https://srldiagnostics.in/)
+- [Tata 1mg](https://www.1mg.com/labs?)
+- [Apollo](https://www.apollohomecare.com/home-visit/)
+"""
+st.header("Full Body Health Packages")
+"""
+- [Fortis Healthcare](https://www.fortishealthcare.com/health-check-packages)
+- [Redcliffe Labs Essential Health](https://redcliffelabs.com/cms-lp/vital-screening-package-lite?network=g&campaignid=21192701230&adgroupid=160746592963&keyword=redcliffe+labs&matchtype=b&creative=696646889002&feeditemid=&device=c&targetid=kwd-1385641867435&adposition=&source=googleads&Location=9182923&gad_source=1&gad_campaignid=21192701230&gbraid=0AAAAABYFFzBzd9ZwcowH0mZbVJRvZhRbb&gclid=Cj0KCQjwjJrCBhCXARIsAI5x66VxFSQEU_7R9CU0B6MJUtmkqC6jA44C1HK7VlGufaChUcZctbU_sWEaAkqpEALw_wcB&wpsrc=Google+Organic+Search)
+- [Thyrocare Aarogyam](https://emailer.thyrocare.com/DM_landing_page/DM_Emailer.aspx?val=UFJPSjEwNTQyNTQsUFJPSjEwNTQyNTYsUFJPSjEwNTQyNTcsUFJPSjEwNTQyNTgsUFJPSjEwNTQyNTksUFJPSjEwNTQyNjAsUFJPSjEwNTQyNjEsUFJPSjEwNTQyNjM=&dQsQa_code=7774834721&gad_source=1&gad_campaignid=21475695119&gbraid=0AAAAADM2NvE7wBH6a8oexZj52yMYgY22h&gclid=Cj0KCQjwjJrCBhCXARIsAI5x66VhBt3iYCQdNKb4MaY0qumqebuImI5sMNTH2gxToCGR8xclFuXJwXsaAlx-EALw_wcB)
+- [Apollo Master Health Checkup]()
+- [Max Healthcare](https://www.maxhealthcare.in/preventive-health-plans#)
+
 """
