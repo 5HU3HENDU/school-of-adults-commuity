35cb305813f937849fb87f5294794761bda7a4ae
Shubhendu Singh <shubhendusingh1505@gmail.com>
2025-06-10 20:55:59 +0530

    Update travel_place.py

--- pages/travel_place.py
+++ pages/travel_place.py
@@ -5,5 +5,23 @@ st.set_page_config(
 )
 
 st.title("🏖️ Places")
+"""
+Place specific informations
+"""
 
 st.divider()
+
+st.header("Kolkata")
+"""
+- Tri Art Gallary :material/location_on:
+- KCC Art gallery :material/location_on:
+- Asia’s largest flower market :material/location_on:
+- Tram Ride :material/location_on:
+- Tiretta Bazaar :material/location_on:
+- James Prinsep Monument & Ghat :material/location_on:
+- Mishti Doi :material/restaurant:
+- Nolen gur sandesh :material/restaurant:
+- KC Das Rosogolla :material/restaurant:
+- Park street :material/location_on:
+
+"""
