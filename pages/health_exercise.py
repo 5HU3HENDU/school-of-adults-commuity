92d47c11a45315bda23da3739f32432685742b61
Israamanaziz <azizisra2001@gmail.com>
2025-06-12 21:34:34 +0530

    Update health_exercise.py

--- pages/health_exercise.py
+++ pages/health_exercise.py
@@ -11,10 +11,39 @@ st.divider()
 st.header("Things to do")
 
 """
-- Walking
+- Walking 
 - Running
+- Swimming
 - [Yoga](https://pocketyoga.com/pose/)
 - [Gym](https://musclewiki.com/)
-- Outdoor Sports such as Swimming, Cricket, Football etc.
+- Outdoor Sports such as Cricket, Football, Tennis, etc.
+- Zumba
+- [Meditation](https://www.mindful.org/how-to-meditate/)
+- [At-home exercises](https://www.healthline.com/health/fitness-exercise/at-home-workouts#intermediate-routine)
+"""
 
+st.header("Movement for Desk jobs")
+"""
+- If your office is nearby, try walking to and from work.
+- Install a [standing desk converter](https://www.ikea.com/in/en/cat/standing-desks-55008/).
+- Use stairs always.
+- Take small breaks every 40 minutes(set timer) and do some stretching exercises: neck rolls, shoulder shrugs and wrist stretches. 
+"""
+
+st.header("Mentally stimulating exercises/activities")
+"""
+- Crosswords & sudoku
+- [Chess](https://www.chess.com/)
+- Reading
+- Drawing
+- Doodling
+- [Zen Tangling](https://zentangle.com/pages/about)
+"""
+
+st.header("Sports and Exercise Products")
+"""
+- [Decathlon](https://www.decathlon.in/)
+- [Sportsjam](https://sportsjam.in/)
+- [Cosco](https://www.cosco.in/)
+- [Powermax](https://www.powermaxfitness.net/?srsltid=AfmBOopNwy1BIpWY_-Kcwuch8PmqubrCBZUoEbc8-gU9ehTOiD-n9T2_)
 """
