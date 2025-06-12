d03d137980300330f144b921461ad1db9ef27380
Israamanaziz <azizisra2001@gmail.com>
2025-06-12 20:13:15 +0530

    Update health_body_parts.py

--- pages/health_body_parts.py
+++ pages/health_body_parts.py
@@ -1,3 +1,74 @@
 import streamlit as st
 
-st.title("Body Parts | SofA")
+st.set_page_config(
+    layout="wide"   
+)
+
+st.title("⚕️Body Parts | SofA")
+
+st.divider()
+
+st.header("🧠Brain And Nervous Sytem")
+"""
+
+"""
+
+st.header("👁️Eyes")
+"""
+
+"""
+
+st.header("ENT(👂🏻Ear,👃🏻Nose and Throat)")
+"""
+
+"""
+
+st.header("🦷Teeth & Oral Health")
+"""
+- Always brush twice a day.
+- Get a full oral health exam every 6-12 months.
+- Use stainless steel tongue scraper for removing bacteria, toxins from tongue.
+- To level-up oral hygiene use [smart brushes](https://www.amazon.in/stores/Oral-BToothbrush/page/368C51AC-6B7A-46EE-8567-46D5EF418614) with pressure sensors and timers
+"""
+
+st.header("🫀Heart")
+"""
+
+"""
+st.header("🫁Lungs & Respiratory System")
+"""
+
+"""
+
+st.header("Liver & Kidneys")
+"""
+
+"""
+
+st.header("🥘Digestive system")
+"""
+
+"""
+
+st.header("Skin, hair & Nails")
+"""
+
+"""
+
+st.header("Reproductive Organs")
+"""
+
+"""
+
+st.header("Hormones")
+"""
+
+"""
+
+st.header("")
+"""
+
+"""
+
+
+
