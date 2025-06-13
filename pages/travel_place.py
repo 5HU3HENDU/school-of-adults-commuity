38dd26e562551b16b14f5ea8446c05d39d6e201c
Israamanaziz <azizisra2001@gmail.com>
2025-06-13 22:14:18 +0530

    Update travel_place.py

--- pages/travel_place.py
+++ pages/travel_place.py
@@ -4,12 +4,21 @@ st.set_page_config(
     layout="wide"   
 )
 
-st.title("🏖️ Places")
+st.title("🏖️ Places | SofA")
 """
-Place specific information
+**Place specific information**
 """
 
 st.divider()
+
+
+st.header("Ludhiana")
+"""
+- Chawala Chicken :material/restaurant:
+- Any Parantha Shop near Clock Tower :material/restaurantlocation_on:
+- Sharmam Shawl Factory :material/location_on:
+- Panditji Di Hatti in Chaura Bazar :material/restaurant:
+"""
 
 st.header("Kolkata")
 """
@@ -23,5 +32,85 @@ st.header("Kolkata")
 - Nolen gur sandesh :material/restaurant:
 - KC Das Rosogolla :material/restaurant:
 - Park street :material/location_on:
+"""
 
+st.header("Lucknow")
+"""
+- The Residency :material/location_on:
+- National Botanical Garden :material/park:
+- Hazratganj :material/restaurantlocation_on:
+- Dilkusha Garden :material/park:
+- Aminabad Bazaar :material/restaurantlocation_on:
+- Ratti Lal(Khasta Kachodi) :material/restaurant:
+- Tundey kebabi :material/restaurant:
+- Raheem's Kulche Nihari :material/restaurant:
+- Ghanta Ghar :material/location_on:
+- Hanumant Dham :material/location_on:
+- Bada Imambara :material/castle:
+- Janeshwar Mishra Park (Asia's Largest Park) :material/Park:
+- Rumi Darwaza  :material/location_on:
+- La Martiniere College  :material/castle:
+- Chowk: Akbari Gate (for Chikan and zardozi works; Idrees Biryani) :material/restaurantlocation_on:
+- Lulu mall (hypermarket food court)  :material/restaurant:
+"""
+
+st.header("Delhi")
+"""
+- Hauz Khas :material/location_on:
+- Majnu ka Tilla(Tibetan Food) :material/restaurant:
+- Jama Masjid :material/location_on:
+- Dilli Haat :material/restaurantlocation_on:
+- Qutub Minar :material/location_on:
+- Red Fort :material/fort:
+- Humayun's tomb :material/location_on:
+- Swaminarayan Akshardham :material/location_on:
+- Lotus Temple :material/location_on:
+- Janpath Market :material/location_on:
+- India Gate :material/location_on:
+"""
+
+st.header("Jodhpur")
+"""
+- Umaid Bhawan :material/location_on:
+- Jaswant Thada :material/castle:
+- Rao Jodha Desert Rock Park :material/location_on:
+- Mehrangarh Fort(Ziplining) :material/fort:
+- Chaudhary - Mirchi Pakoda :material/restaurant:
+- Janta Sweet Home - kachodi/Jaleba :material/restaurant:
+- Mandore Garden :material/location_on:
+- Blue City Walk near Singhvi Haveli :material/location_on:
+- Toorji ka Jhalra Bavdi :material/location_on:
+- Arora Chat- Palak Patta Chat :material/restaurant:
+- Shri Misri Lal-Lassi :material/restaurant:
+- Book Cafe (open until 3am) :material/restaurant:
+- Singhoria Hills :material/location_on:
+- Ghanta Ghar/Sardar market :material/restaurantlocation_on:
+- Nehru park( Bun makhan, paneer pakoda) :material/restaurantpark:
+- Ghasmandi (rawat ka Munim, Ram Namkeen wala) :material/restaurantlocation_on:
+"""
+st.header("Amritsar")
+"""
+- Kesar Dhaba :material/restaurant:
+- Brothers dhaba (Prahvan da dhaba) :material/restaurant:
+- Bhai Kulwant Singh Kulchey Wale :material/restaurant:
+- Haveli :material/restaurant:
+- Makhan Fish and Chicken Corner :material/restaurant:
+- A-one Kulfa :material/icecream:
+- Masala darbar :material/restaurant:
+- Ramdas Jalebi Wale :material/restaurant:
+- Gian Chand di lassi :material/restaurant:
+"""
+
+st.header("Surat")
+"""
+- Surat Fort :material/fort:
+- Gopal Locho (Sev Khamanis)  :material/restaurant:
+- Chirag Nankhatai :material/restaurant:
+- Dabelis Fafdas :material/restaurant:
+- Dumas beach (black sand) :material/location_on:
+- Gopi talav :material/location_on:
+- Bombay Market :material/location_on:
+- Baroda Market (clothes at cheap prices):material/location_on:
+- Swaminarayan Temple :material/location_on:
+- Shah Jamnadas Ghariwala (Gharis) :material/restaurant:
 """
