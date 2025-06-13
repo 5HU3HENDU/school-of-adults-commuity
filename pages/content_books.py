4100430d45816252af7a569906aef90cc9601279
Israamanaziz <azizisra2001@gmail.com>
2025-06-13 16:48:03 +0530

    Update content_books.py

--- pages/content_books.py
+++ pages/content_books.py
@@ -1,12 +1,28 @@
 import streamlit as st
 
+st.set_page_config(
+    layout="wide"
+)
+
 st.title("📚 Books | SofA")
 
 st.divider()
 
 """
 - [Meet New Books ](https://www.meetnewbooks.com/)
-
 """
 
+st.header("Bookstores")
+"""
+- [Crossword](https://www.crossword.in/)
+- [Bookchor](https://www.bookchor.com/)
+- [BooksWagon](https://www.bookswagon.com/)
+"""
+
+st.header("Indian Culture")
+"""
+- [E-Books on Indian culture](https://indianculture.gov.in/ebooks)
+- [The Indian Book Club](https://theindianbookclub.com/)
+- [National Digital Library of India](https://ndl.iitkgp.ac.in/)
+"""
 
