96fd66c51ace1d129a85ec5a84b9d2de912b9cb6
Israamanaziz <azizisra2001@gmail.com>
2025-06-13 20:08:15 +0530

    Update travel_flight.py

--- pages/travel_flight.py
+++ pages/travel_flight.py
@@ -11,11 +11,17 @@ st.divider()
 """
 - [Book Flights](https://www.ixigo.com/flights)
 - [Airport SuperApp Kind of](https://www.adanione.com/)
-
 """
 
 st.header("Things to avoid")
 """
-- Do not buy tickets and book hotels before having the visa.
+- Do not book flight tickets and hotels before your visa is approved.
+- Avoid booking non-refundable tickets for first-time international travel.
+- Never rely on verbal confirmations, document everything using email or receipt.
+"""
+
+st.header("Airport Know-Hows")
+"""
 - Store your boarding pass on [Digiyatra](https://digiyatrafoundation.com/) for smooth airport entry to boarding gates. It keeps track of all your flight tickets too.
+- Keep documents in one place, use apps like Google Drive, Apple Wallet or Notion to store passport scans, visa, Aadhar card etc.
 """
