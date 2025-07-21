397acc8168efb38159f71d0effa5cff2dfcaf12a
Shubhendu Singh <shubhendusingh1505@gmail.com>
2025-07-21 22:11:58 +0530

    Update life_event_buying_home.py

--- pages/life_event_buying_home.py
+++ pages/life_event_buying_home.py
@@ -1,2 +1,16 @@
 import streamlit as st
 
+st.set_page_config(
+    layout="wide"   
+)
+
+st.title("🏡 Buying House")
+
+st.divider()
+
+st.header("Buying vs Renting")
+"""
+- [Buying vs Renting Calculator](https://fi.money/calculators/rent-vs-buy-calculator)
+"""
+
+
